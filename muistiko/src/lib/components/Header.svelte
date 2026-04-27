317729093b0ac8cbfca58ae5f27b60ade4ae1387
Roni <ah2825@student.jamk.fi>
2026-04-27 10:16:03 +0300

    add Header contents

---
+++ muistiko/src/lib/components/Header.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+ import PeliInfo from "../../routes/peliInfo.svelte";
+ import Button from "./Button.svelte";
+	interface Props {
+		pelinLogo: string;
+	}
+
+	let { pelinLogo }: Props = $props();
+	let naytaInfo = $state(false)
+</script>
+
+<header>
+	{#if pelinLogo}
+		<img src={pelinLogo} alt="" height="75%" />
+	{/if}
+	<Button text="info" onclick={()=>naytaInfo = true} />
+	
+ {#if naytaInfo}
+ <PeliInfo  suljeInfo={()=> naytaInfo = false}/>
+
+ {/if}
+
+</header>
+
+<style>
+	header {
+		padding: 2rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: #aaa;
+		height: 150px;
+	}
+
+	img {
+		max-height: 100%;
+		max-width: 100%;
+		object-fit: contain;
+	}
+</style>
