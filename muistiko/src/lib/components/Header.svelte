122fe3fc0375103f7ae1eca3bb079e94a5c3e53c
Roni Huuskonen <ah2825@student.jamk.fi>
2026-04-28 10:32:43 +0300

    modified Header

--- muistiko/src/lib/components/Header.svelte
+++ muistiko/src/lib/components/Header.svelte
@@ -1,33 +1,38 @@
 <script lang="ts">
- import PeliInfo from "../../routes/peliInfo.svelte";
- import Button from "./Button.svelte";
+	import PeliInfo from '../../routes/peliInfo.svelte';
+	import Button from './Button.svelte';
 	interface Props {
 		pelinLogo: string;
 	}
 
 	let { pelinLogo }: Props = $props();
-	let naytaInfo = $state(false)
+	let naytaInfo = $state(false);
 </script>
 
 <header>
 	{#if pelinLogo}
 		<img src={pelinLogo} alt="" height="75%" />
 	{/if}
-	<Button text="info" onclick={()=>naytaInfo = true} />
-	
- {#if naytaInfo}
- <PeliInfo  suljeInfo={()=> naytaInfo = false}/>
+	<nav>
+		<div class="navigation">
+			<div class="homebutton">
+				<Button text="Home" onclick={() => (window.location.href = '/')} />
+			</div>
 
- {/if}
+			<div class="infobutton">
+				<Button text="info" onclick={() => (naytaInfo = true)} />
+			</div>
+		</div>
+	</nav>
 
+	{#if naytaInfo}
+		<PeliInfo suljeInfo={() => (naytaInfo = false)} />
+	{/if}
 </header>
 
 <style>
 	header {
 		padding: 2rem;
-		display: flex;
-		align-items: center;
-		justify-content: center;
 		background-color: #aaa;
 		height: 150px;
 	}
@@ -36,5 +41,15 @@
 		max-height: 100%;
 		max-width: 100%;
 		object-fit: contain;
+	}
+
+	.infobutton {
+		border: 2px solid black;
+	}
+
+	nav {
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
 	}
 </style>
