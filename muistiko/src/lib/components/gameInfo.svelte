8c91dd39d67952ea0a04465e9e9935eae78dff57
ville rossi <ah4514@student.jamk.fi>
2026-04-29 13:12:45 +0300

    infomodal updated

--- muistiko/src/lib/components/gameInfo.svelte
+++ muistiko/src/lib/components/gameInfo.svelte
@@ -12,20 +12,29 @@
 <Modal>
 	{#snippet header()}
 	<div class="header-content">
-		<h1>MuisTiko Info</h1>
+		<h1><strong>INFO</strong></h1>
 		<div class="close-button">
 		<Button  onclick={closeInfo} text="×"/>
 		</div>
 	</div>
 	{/snippet}
-
+   
 	{#snippet content()}
+	<p><strong>MuisTiko</strong> on muistipeli, jossa tavoitteena on löytää kaikki korttiparit.</p>
+	<h3><strong>Säännöt:</strong></h3>
 		<ul>
-			<li>Tarkoitus on saada kerättyä kaikki parit mahdollisimman vähillä siirroilla</li>
-			<li>....</li>
-			<li>.....</li>
-			<li>.....</li>
+			<li>Klikkaa kortteja löytääksesi parit</li>
+			<li>Kun löydät parin, kortit jäävät näkyviin</li>
+		  <li>Peli päättyy kun kaikki parit on löydetty</li>
+			<li>Yritä löytää kaikki parit mahdollisimman vähillä siirroilla</li>	
 		</ul>
+
+ <h3><strong>Toiminnot:</strong></h3>
+	<ul>
+		<li><strong>Teema:</strong> 4 eri teemaa, joista voit valita mieluisan, musiikki vaihtuu teeman kanssa.</li>
+		<li><strong>Vaikeustaso:</strong> 4 eri vaikeustasoa mistä voit valita</li>
+		<li><strong>Aikaraja:</strong> Voit vaikeuttaa peliä asettamalla aikarajan</li>
+	</ul>
 	{/snippet}
 
 	{#snippet footer()}
