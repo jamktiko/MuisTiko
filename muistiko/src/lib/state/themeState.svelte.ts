6b4e4dbd4f7b8c79ec23c0691e74d1dd2cb442d9
ville rossi <ah4514@student.jamk.fi>
2026-04-30 10:05:35 +0300

    music now starts from pelaa button + themeState edited

--- muistiko/src/lib/state/themeState.svelte.ts
+++ muistiko/src/lib/state/themeState.svelte.ts
@@ -1,13 +1,29 @@
-/* <import type {Theme}  from "./gameState.svelte";
 
-export const themeSetting: Record<Theme, {
-  music: string,
-  colors: {
-    header: string
-    footer: string
-    card: string
-    buttons: string
-    background: string
-    text: string
-  }
-}> =  */
+
+type themeColors = {
+  background: string
+  header: string
+  footer: string
+  card: string
+  button: string
+  text: string
+}
+class themeSetting {
+   name: string
+  music: string
+  colors: themeColors
+
+constructor(name: string, music: string, colors: themeColors) {
+this.name = name
+this.music = music
+this.colors = colors
+}
+} 
+
+/*export const Themes: themeSetting[] = [
+ new themeSetting('Kissat', 'villatic_music-cute-cat-256893.mp3', {
+
+ }
+
+ )
+]*/
